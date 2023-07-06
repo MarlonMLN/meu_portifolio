1230936d3800c7a4f51f95cd0b91d129484fdf91
Marlon marques vieira <marlon.mmv.v@gmail.com>
2023-07-06 07:47:37 -0300

    first commit

---
+++ frontend/public/src/index.ts
@@ -0,0 +1,109 @@
+body {
+    background-color: #f8f8f8;
+    color: #333;
+    font-family: Arial, sans-serif;
+  }
+  
+  /* Header styles */
+
+  .nav_primaria a {
+    color: #2b2e3a;
+    
+  }
+  
+  header {
+    background-color: #fff;
+    padding: 20px;
+    text-align: center;
+    font-family: 'Poppins', sans-serif;
+  }
+  
+  header h1 {
+    font-size: 36px;
+    margin: 20px 0;
+  }
+  
+  header p {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+
+  header span {
+    color: #d22650;
+    font-weight: bold;
+  }
+  
+  header p hr {
+    color: #2b2e3a;
+    font-weight: bold;
+  }
+  
+  header nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  
+  header nav ul li {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  
+  header nav ul li a {
+    color: #fff;
+    text-decoration: none;
+    padding: 10px;
+    transition: color 0.3s ease;
+  }
+  
+  header nav ul li a:hover {
+    color: #d22650;
+  }
+
+
+  
+  .download a {
+    display: inline-block;
+    background-color: #c01111;
+    color: #c01111;
+    text-decoration: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    margin-top: 10px;
+    transition: background-color 0.3s ease;
+  }
+
+  
+  
+ 
+  /* REDES SOCIAIS*/
+  .contact-icon {
+    margin-top: 20px;
+  }
+  
+  .contact-icon a {
+    display: inline-block;
+    border-radius: 50%;
+    height: 20px;
+    margin: 8px;;  
+    color: #db080813;
+    font-size: 24px;
+  }
+  
+  
+  .contact-icon a:hover {
+    color: #333;
+  }
+  
+  /* Footer styles */
+  footer {
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+  }
+  
+  footer p {
+    margin: 0;
+  }
+  
